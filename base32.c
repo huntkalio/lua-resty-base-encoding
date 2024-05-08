596bd3ae7f5f716d78b4faa56ae310b1c95bcaca
huntkalio <51730550+huntkalio@users.noreply.github.com>
2024-05-08 19:30:13 +0800

    Update base32.c

--- base32.c
+++ base32.c
@@ -186,3 +186,101 @@ size_t b32_decode(char *dest, const char *src, size_t len, uint32_t hex)
 
     return (size_t)(p - (uint8_t*)dest) + step;
 }
+
+int int_b32_decode(char *dest, const char *src, size_t len, uint32_t hex)
+{
+    unsigned char        in1, in2, in3, in4, in5, in6, in7, in8;
+    const unsigned char *s = (const unsigned char*)src;
+    unsigned char       *p = (unsigned char*)dest;
+
+    const char *decode_table = (hex == 0) ? std_decode_table : hex_decode_table;
+
+    if (src[len - 1] == CHARPAD) {
+        /*
+        * if padding is used, then the message must be at least
+        * 8 chars and be a multiple of 8
+        */
+        if (len < 8 || (len % 8 != 0)) {
+            return -1;
+        }
+
+        len--;
+        /* there can be at most 7 pad chars at the end */
+        int i = 0;
+        for (i = 0; src[len - 1] == CHARPAD; i++) {
+            if (i >= 6) {
+                return -1;
+            }
+            len--;
+        }
+    }
+
+    while (likely(len >= OUT_BLOCK_LEN)) {
+        in1 = decode_table[*s++];
+        in2 = decode_table[*s++];
+        in3 = decode_table[*s++];
+        in4 = decode_table[*s++];
+        in5 = decode_table[*s++];
+        in6 = decode_table[*s++];
+        in7 = decode_table[*s++];
+        in8 = decode_table[*s++];
+
+        /* faster than memchr */
+        if (unlikely(in1 == BADCHAR || in2 == BADCHAR || in3 == BADCHAR
+            || in4 == BADCHAR || in5 == BADCHAR || in6 == BADCHAR
+            || in7 == BADCHAR || in8 == BADCHAR))
+        {
+            return -1;
+        }
+
+        *p++ = ((in1 & 0x1f) << 3) | ((in2 & 0x1c) >> 2);
+        *p++ = ((in2 & 0x03) << 6) | ((in3 & 0x1f) << 1) | ((in4 & 0x10) >> 4);
+        *p++ = ((in4 & 0x0f) << 4) | ((in5 & 0x1e) >> 1);
+        *p++ = ((in5 & 0x01) << 7) | ((in6 & 0x1f) << 2) | ((in7 & 0x18) >> 3);
+        *p++ = ((in7 & 0x07) << 5) | (in8 & 0x1f);
+
+        len -= OUT_BLOCK_LEN;
+    }
+
+    int step = 0;
+    unsigned int i;
+    for (i = 0; i < len; i++) {
+        if ((unsigned char)decode_table[s[i]] == BADCHAR) {
+            return -1;
+        }
+    }
+
+    switch(len) {
+    case 7:
+        in5 = decode_table[s[4]];
+        in6 = decode_table[s[5]];
+        in7 = decode_table[s[6]];
+        p[3] = ((in5 & 0x01) << 7) | ((in6 & 0x1f) << 2) | ((in7 & 0x18) >> 3);
+        step++;
+        /* fall through */
+    case 5:
+        in5 = decode_table[s[4]];
+        in4 = decode_table[s[3]];
+        p[2] = ((in4 & 0x0f) << 4) | ((in5 & 0x1e) >> 1);
+        step++;
+        /* fall through */
+    case 4:
+        in4 = decode_table[s[3]];
+        in3 = decode_table[s[2]];
+        in2 = decode_table[s[1]];
+        p[1] = ((in2 & 0x03) << 6) | ((in3 & 0x1f) << 1) | ((in4 & 0x10) >> 4);
+        step++;
+        /* fall through */
+    case 2:
+        in2 = decode_table[s[1]];
+        in1 = decode_table[s[0]];
+        p[0] = ((in1 & 0x1f) << 3) | ((in2 & 0x1c) >> 2);
+        step++;
+    case 0:
+        break;
+    default:
+        return -1;
+    }
+
+    return (int)(p - (uint8_t*)dest) + step;
+}
